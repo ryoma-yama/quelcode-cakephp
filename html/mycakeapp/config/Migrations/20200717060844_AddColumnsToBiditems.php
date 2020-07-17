b6af61f9793bd2bd31cab5ce85fd09f6a1dff1ba
RyomaY <docomoknps200uu@gmail.com>
2020-07-17 15:11:42 +0900

    Add: biditemテーブルにmodifiedカラムを追加

---
+++ html/mycakeapp/config/Migrations/20200717060844_AddColumnsToBiditems.php
@@ -0,0 +1,22 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AddColumnsToBiditems extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('biditems');
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->update();
+    }
+}
