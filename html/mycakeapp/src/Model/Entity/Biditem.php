9621c86b12e5c9472722e1ccb8d6ef6a64c5c584
RyomaY <docomoknps200uu@gmail.com>
2020-07-17 15:45:04 +0900

    Add: Biditem.phpの$_accssibleに, descriptionカラムを追加

--- html/mycakeapp/src/Model/Entity/Biditem.php
+++ html/mycakeapp/src/Model/Entity/Biditem.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
@@ -37,5 +38,6 @@ class Biditem extends Entity
         'user' => true,
         'bidinfo' => true,
         'bidrequests' => true,
+        'description' => true,
     ];
 }
