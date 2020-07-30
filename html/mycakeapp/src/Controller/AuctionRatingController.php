80569eb63ef89eae794b43ed14fd571b9d7ac53b
RyomaY <docomoknps200uu@gmail.com>
2020-07-30 12:02:07 +0900

    Add: 取引評価ページへのアクセス制御を追加

--- html/mycakeapp/src/Controller/AuctionRatingController.php
+++ html/mycakeapp/src/Controller/AuctionRatingController.php
@@ -26,5 +26,21 @@ class AuctionRatingController extends AuctionBaseController
 
     public function addRate($bidinfo_id = null)
     {
+        try {
+            $bidinfo = $this->Bidinfo->get($bidinfo_id, [
+                'contain' => ['Biditems']
+            ]);
+            $seller = $bidinfo->biditem->user_id;
+            $buyer = $bidinfo->user_id;
+            if ($seller === $this->Auth->user('id')) {
+                $this->set('is_seller', true);
+            } else if ($buyer === $this->Auth->user('id')) {
+                $this->set('is_buyer', true);
+            } else {
+                return $this->redirect(['controller' => 'Auction', 'action' => 'index']);
+            }
+        } catch (Exception $e) {
+            return $this->redirect(['controller' => 'Auction', 'action' => 'index']);
+        }
     }
 }
