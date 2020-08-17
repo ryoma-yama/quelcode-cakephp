c9c7f2bd73cde31857e63b4d61cd80729055bb09
RyomaY <docomoknps200uu@gmail.com>
2020-08-17 11:45:52 +0900

    Fix: app.phpをvagrant用に修正

--- html/mycakeapp/config/app.php
+++ html/mycakeapp/config/app.php
@@ -256,21 +256,21 @@ return [
             'className' => Connection::class,
             'driver' => Mysql::class,
             'persistent' => false,
-            'host' => 'mysql',
+            'host' => 'localhost',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'docker_db_user',
-            'password' => 'docker_db_user_pass',
-            'database' => 'docker_db',
+            'username' => 'root',
+            'password' => '',
+            'database' => 'cake',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
             //'encoding' => 'utf8mb4',
-            'timezone' => 'Asia/Tokyo',
+            'timezone' => '+09:00',
             'flags' => [],
             'cacheMetadata' => true,
             'log' => false,
